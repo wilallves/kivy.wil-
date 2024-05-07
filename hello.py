24de49a66ca4d73807265b9e9c49882047d7364c
wil ꩜ <78051450+wilallves@users.noreply.github.com>
2024-05-07 17:20:49 -0300

    hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+from kivy.app import App 
+from kivy.uix.button import Button 
+
+class MyApp(App):
+    def build(self):
+        return Button(text="Hello World! This is my first program in kivy \n I'm a SESIANO Student, and I love me")
+
+if __name__ == '__main__':
+    MyApp().run()
